f79712733988b608223472ea2f9ba9481395f59a
unknown <cr.jetstream@gmail.com>
2021-02-20 20:22:39 -0500

    AWG update

--- Run_AWG.cmd
+++ Run_AWG.cmd
@@ -1,5 +1,5 @@
 echo off 
 call .\venv\Scripts\activate.bat
-python create_thumbnail.py -e AWG -n Winter_Split_3 -o log_file.txt
+python create_thumbnail.py -e AWG -n Winter_Split_4 -o log_file.txt
 type log_file.txt
 pause
