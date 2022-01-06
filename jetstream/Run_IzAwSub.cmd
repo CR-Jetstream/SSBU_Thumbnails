c99b7311bcfeeb272f8b89be806d013b437c7793
unknown <cr.jetstream@gmail.com>
2022-01-06 08:47:58 -0500

    updating to include up to Quaratianmnet 85

--- jetstream/Run_IzAwSub.cmd
+++ jetstream/Run_IzAwSub.cmd
@@ -1,5 +1,5 @@
 echo off 
 call ..\venv\Scripts\activate.bat
-python create_thumbnail.py -e "IzAw Sub" -n "Random" -o missing.log
+python create_thumbnail.py -e "IzAw Sub" -n "4" -o missing.log
 type missing.log
 pause
--- jetstream/Run_Jetstream.cmd
+++ jetstream/Run_Jetstream.cmd
@@ -1,5 +1,5 @@
 echo off 
 call ..\venv\Scripts\activate.bat
-python create_thumbnail.py -e Quarantainment -n 82 -o missing.log
+python create_thumbnail.py -e Quarantainment -n 85 -o missing.log
 type missing.log
 pause
