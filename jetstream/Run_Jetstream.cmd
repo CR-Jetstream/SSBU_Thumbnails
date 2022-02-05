efadb48b13149f3e281679b08e916c3a1b25c121
unknown <cr.jetstream@gmail.com>
2022-02-05 11:57:11 -0500

    Added Q87 & Q88

--- jetstream/Run_Jetstream.cmd
+++ jetstream/Run_Jetstream.cmd
@@ -1,5 +1,5 @@
 echo off 
 call ..\venv\Scripts\activate.bat
-python create_thumbnail.py -e Quarantainment -n 85 -o missing.log
+python create_thumbnail.py -e Quarantainment -n 87 -o missing.log
 type missing.log
 pause
