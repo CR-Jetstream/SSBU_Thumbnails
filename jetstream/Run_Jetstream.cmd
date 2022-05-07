9340033a97fb8857ee1e52e5aa3cb8eeaf48a3d0
unknown <cr.jetstream@gmail.com>
2022-05-07 13:39:37 -0400

    Q90 update

--- jetstream/Run_Jetstream.cmd
+++ jetstream/Run_Jetstream.cmd
@@ -1,5 +1,5 @@
 echo off 
 call ..\venv\Scripts\activate.bat
-python create_thumbnail.py -e Quarantainment -n 88 -o missing.log
+python create_thumbnail.py -e Quarantainment -n 90 -o missing.log
 type missing.log
 pause
